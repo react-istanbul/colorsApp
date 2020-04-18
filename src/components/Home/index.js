a122b9b4d54cff13a3cab2a33154fccaca42277a
Sinan Aksay <me@wunnle.com>
2020-04-18 17:22:31 +0300

    created home structure

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import styles from './Home.module.css' 
 import { useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
+import Header from '../Header'
+import styles from './Home.module.css'
+
 const Home = () => {
-
-  const isUserLoggedIn = useSelector(s => s.login.isUserLoggedIn)
-
-  if(!isUserLoggedIn) {
-    return <Redirect to='/login' />
-  }
+  const isUserLoggedIn = useSelector((s) => s.login.isUserLoggedIn)
 
   return (
-    <div className={styles.home}>This is home</div>
+    <div className={styles.home}>
+      <Header />
+      <div className={styles.cardsHolder}>there will be some cards here</div>
+    </div>
   )
 }
-
 
 export default Home
