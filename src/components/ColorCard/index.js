86365cd91ac711ad196565b079e92cba6f2a1075
Sinan Aksay <me@wunnle.com>
2020-05-02 17:38:59 +0300

    added click to copy functionality

--- src/components/ColorCard/index.js
+++ src/components/ColorCard/index.js
@@ -1,26 +1,47 @@
+import Copy from 'copy-text-to-clipboard'
 import PropTypes from 'prop-types'
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 import styles from './ColorCard.module.css'
 
-const ColorCard = ({ name, year, pantone_value, color }) => (
-  <div className={styles.colorCard}>
-    <div className={styles.preview} style={{ backgroundColor: color }}>
-      <span className={styles.hex}>{color}</span>
-    </div>
-    <div className={styles.decription}>
-      <div className={styles.name}>{name}</div>
-      <div className={styles.info}>
-        <span className={styles.infoTitle}>Pantone value</span>
-        <span className={styles.infoValue}>{pantone_value}</span>
+const ColorCard = ({ name, year, pantone_value, color }) => {
+  const [isCopied, setCopied] = useState(false)
+
+  function handleClickPreview() {
+    Copy(color)
+    setCopied(true)
+
+    setTimeout(() => {
+      setCopied(false)
+    }, 1000)
+  }
+
+  return (
+    <div className={styles.colorCard}>
+      <div
+        className={styles.preview}
+        style={{ backgroundColor: color }}
+        onClick={handleClickPreview}
+      >
+        <span className={styles.hex}>{isCopied ? 'Copied!' : color}</span>
+        {!isCopied && <span className={styles.hoverText}>Click to copy</span>}
       </div>
-      <div className={styles.info}>
-        <span className={styles.infoTitle}>Year</span>
-        <span className={styles.infoValue}>{year}</span>
+      <div className={styles.decription}>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.info}>
+          <span className={styles.infoTitle}>Pantone value</span>
+          <span className={styles.infoValue}>{pantone_value}</span>
+        </div>
+        <div className={styles.info}>
+          <span className={styles.infoTitle}>Year</span>
+          <span className={styles.infoValue}>{year}</span>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 ColorCard.propTypes = {
   name: PropTypes.string.isRequired,
